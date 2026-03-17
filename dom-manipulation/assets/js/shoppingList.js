592377d9fb828de65506de7170b9ab86371b7a68
Marco-Petronella <marco.petronella85@gmail.com>
2026-03-17 11:52:56 +0100

    aggiunta logica per la gestione della lista della spesa con funzionalità di completamento elementi

--- dom-manipulation/assets/js/shoppingList.js
+++ dom-manipulation/assets/js/shoppingList.js
@@ -1,23 +1,31 @@
 const addElementButton = document.getElementById("add-shopping-element");
 const inputShopping = document.getElementById("input-shopping-list");
-const shoppingList = document.getElementById("shopping-list")
+const shoppingList = document.getElementById("shopping-list");
 console.log(addElementButton);
 console.log(inputShopping);
 console.log(shoppingList);
 
 const shoppingListArray = [];
 
-addElementButton.addEventListener('click', function() {
- const shoppingElement = inputShopping.value;
- if (shoppingElement.length > 0) {
+addElementButton.addEventListener("click", function (e) {
+  e.preventDefault(); //evita lo scroll in alto usando l'"a" come bottone
+  const shoppingElement = inputShopping.value;
+  if (shoppingElement.length > 0) {
     shoppingListArray.push(shoppingElement);
-    updateShoppingList();
- }
-})
-function updateShoppingList() {
-    let shoppingListHTML= "lista attuale: "
-    for (let i=0; i<shoppingListArray.length; i++) {
-        shoppingListHTML+=`<li>${shoppingListArray[i]}</li>`
-    }
-    shoppingList.innerHTML=shoppingListHTML;
+    updateShoppingList(shoppingElement);
+  }
+  inputShopping.value = ""; //svuota l'input
+});
+function updateShoppingList(shoppingElement) {
+  let i = shoppingListArray.length;
+  const li = document.createElement("li");
+  li.classList.add("shopping-item");
+  li.id = "shoppingListArray" + i;
+  li.textContent = shoppingElement;
+  shoppingList.appendChild(li);
+  console.log(shoppingListArray);
+  li.addEventListener('click', function () {
+    // li.style.textDecoration = "line-through";
+    li.classList.toggle("completed"); //mette o toglie la classe se esiste
+  })
 }
